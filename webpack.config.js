a6f3b57bb54fb9aba587604dc38281f26972510d
Hendrik Lammers <lammers@gmail.com>
2017-01-29 00:03:42 +0100

    Update webpack config for v2

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,39 @@
 var path = require('path')
 
 module.exports = {
+  context: path.resolve(__dirname, 'src'),
   devtool: 'source-map',
   entry: [
     'babel-polyfill',
-    path.resolve(__dirname, 'src/index.js')],
+    path.resolve(__dirname, 'src/index.js')
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/',
+    publicPath: '/dist',
     filename: 'bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.(js)$/,
-        loader: 'babel-loader',
+        test: /\.js$/,
         include: [
           path.resolve(__dirname, 'src')
-        ]
+        ],
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'stage-0']
+          }
+        }]
       }
     ]
+  },
+  performance: {
+    hints: false
+  },
+  devServer: {
+    contentBase: __dirname + '/public',
+    inline: true,
+    port: 8000
   }
 }

3f71b19a4a10b43dd6eb2f82fe5c3fa024fb8a9d
Hendrik Lammers <lammers@gmail.com>
2017-12-09 16:32:16 +0700

    Move dist to public folder

--- webpack.config.js
+++ webpack.config.js
@@ -3,45 +3,46 @@ const path = require('path')
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   devtool: 'source-map',
-  entry: [
-    path.resolve(__dirname, 'src/index.js')
-  ],
+  entry: [path.resolve(__dirname, 'src/index.js')],
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public/dist'),
     publicPath: '/dist',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: [
-          path.resolve(__dirname, 'src')
+        include: [path.resolve(__dirname, 'src')],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: {
+                      browsers: ['last 2 versions'],
+                    },
+                    useBuiltIns: 'usage',
+                  },
+                ],
+                '@babel/stage-0',
+              ],
+            },
+          },
         ],
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', {
-                targets: {
-                  browsers: ['last 2 versions']
-                },
-                useBuiltIns: 'usage'
-              }],
-              '@babel/stage-0'
-            ]
-          }
-        }]
       },
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
-    ]
+    ],
   },
   devServer: {
     contentBase: __dirname + '/public',
     inline: true,
-    port: 8000
-  }
+    port: 8000,
+  },
 }
